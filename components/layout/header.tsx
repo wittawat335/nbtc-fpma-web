80ea62823d2b68361fa8efb9c6463eb36528197c
Wittawat_F <wittawat.f@softdebut.com>
2026-01-06 16:04:45 +0700

    ีupdate header, nav

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Navbar from "@/components/layout/navbar";
 import dynamic from "next/dynamic";
 const UserMenu = dynamic(() => import("./user-menu"), {
   ssr: false,
@@ -13,19 +14,42 @@ const UserMenu = dynamic(() => import("./user-menu"), {
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 bg-white shadow-sm">
-      <div className="container mx-auto flex items-center justify-between px-4 py-3">
-        <div className="flex items-center gap-4">
-          <h1 className="text-sm font-semibold text-gray-700 md:text-base">
-            ระบบจัดการโครงการกองทุนวิจัยและพัฒนาฯ{" "}
-            <span className="hidden text-blue-600 sm:inline">
-              สำนักงาน กสทช.
-            </span>
-          </h1>
+      <div className="mx-auto max-w-7xl px-4 py-3">
+        <div className="flex flex-wrap items-center">
+          <div className="w-full px-2 md:w-1/3 lg:w-auto">
+            <div className="flex items-center justify-center">
+              <a href="/?lang=en-US">
+                <img
+                  src="/logo_NBTC.png"
+                  className="h-[88px] w-auto"
+                  alt="NBTC"
+                />
+              </a>
+              <div className="mx-3 h-[60px] border-l border-gray-300"></div>
+              <a href="/?lang=en-US">
+                <img
+                  src="/Logo_FPMA.webp"
+                  className="h-[88px] w-auto"
+                  alt="FPMA"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="w-full text-center md:w-2/3 md:text-right lg:w-auto lg:text-left">
+            <h1 className="text-2xl leading-tight font-semibold text-[#0082bf]">
+              ระบบจัดการโครงการกองทุนวิจัยและพัฒนาฯ{" "}
+              <span className="hidden sm:inline">สำนักงาน กสทช.</span>
+            </h1>
+          </div>
+          <div className="ml-auto w-full lg:w-auto">
+            <div className="flex items-center justify-center">
+              <UserMenu />
+            </div>
+          </div>
         </div>
-
-        <div>
-          <UserMenu />
-        </div>
+      </div>
+      <div className="TopMenu">
+        <Navbar />
       </div>
     </header>
   );
---
+++ components/layout/navbar.tsx
@@ -0,0 +1,269 @@
+"use client";
+
+import { useState } from "react";
+import { useAuth } from "@/hooks/useAuth";
+import { LogOut, User, Loader2 } from "lucide-react";
+
+export default function Navbar() {
+  const { user, logout, isLoading } = useAuth();
+  const [openKey, setOpenKey] = useState<string | null>(null);
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  //   useEffect(() => {
+  //     document.body.style.overflow = mobileOpen ? "hidden" : "";
+  //     return () => {
+  //       document.body.style.overflow = "";
+  //     };
+  //   }, [mobileOpen]);
+
+  const toggle = (key: string) => {
+    setOpenKey((prev) => (prev === key ? null : key));
+  };
+
+  const closeAll = () => {
+    setOpenKey(null);
+    setMobileOpen(false);
+  };
+
+  if (user) {
+    return (
+      <div className="bg-[#0095DB] text-white">
+        <style>{`
+        .subMenu {
+            position: relative;
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .subMenu::after {
+            content: "";
+            background-image: url(../arrow_dropdown.webp);
+            width: 12px;
+            border: 0;
+            height: 12px;
+            vertical-align: inherit;
+            background-size: 12px;
+            transition: all .1s;
+            margin-left: 8px;
+        }
+        .subMenu.opened::after {
+            transform: rotate(180deg);
+        }
+      `}</style>
+        <div className="mx-auto max-w-7xl px-4">
+          <nav className="relative flex h-[46px] items-center justify-between">
+            <button
+              className="h-[46px] px-4 md:hidden"
+              onClick={() => setMobileOpen(!mobileOpen)}
+            >
+              ☰
+            </button>
+            <ul
+              className={`md:flex ${mobileOpen ? "block" : "hidden"} absolute top-full left-0 z-50 w-full bg-[#0095DB] md:static md:w-auto md:bg-transparent`}
+            >
+              <li>
+                <a
+                  href="#หน้าแรก"
+                  onClick={closeAll}
+                  className="flex h-[46px] cursor-pointer items-center px-[20px] hover:bg-[#05547E]"
+                >
+                  หน้าแรก
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#ประกาศรับข้อเสนอ"
+                  onClick={closeAll}
+                  className="flex h-[46px] cursor-pointer items-center px-[20px] hover:bg-[#05547E]"
+                >
+                  ประกาศรับข้อเสนอ
+                </a>
+              </li>
+              <li className="relative">
+                <button
+                  type="button"
+                  onClick={() => toggle("proposal")}
+                  className={`subMenu flex h-[46px] w-full cursor-pointer items-center px-[20px] hover:bg-[#05547E] md:w-auto ${openKey === "proposal" ? "opened bg-[#05547E]" : ""}`}
+                >
+                  ข้อเสนอโครงการ
+                </button>
+
+                {openKey === "proposal" && (
+                  <ul
+                    className={` ${mobileOpen ? "static" : "absolute"} top-full left-0 min-w-[220px] cursor-pointer bg-[#05547E]`}
+                  >
+                    <li>
+                      <a
+                        href="#ข้อเสนอโครงการ"
+                        onClick={closeAll}
+                        className="block px-4 py-2 hover:bg-[#033A57]"
+                      >
+                        ข้อเสนอโครงการ
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#ผลตรวจความซ้ำซ้อน"
+                        onClick={closeAll}
+                        className="block px-4 py-2 hover:bg-[#033A57]"
+                      >
+                        ผลตรวจความซ้ำซ้อน
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#กำหนดสิทธิ์ข้อเสนอโครงการ"
+                        onClick={closeAll}
+                        className="block px-4 py-2 hover:bg-[#033A57]"
+                      >
+                        กำหนดสิทธิ์ข้อเสนอโครงการ
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#แบบร่างข้อเสนอโครงการ"
+                        onClick={closeAll}
+                        className="block px-4 py-2 hover:bg-[#033A57]"
+                      >
+                        แบบร่างข้อเสนอโครงการ
+                      </a>
+                    </li>
+                  </ul>
+                )}
+              </li>
+              <li className="relative">
+                <button
+                  type="button"
+                  onClick={() => toggle("report")}
+                  className={`subMenu flex h-[46px] w-full cursor-pointer items-center px-[20px] hover:bg-[#05547E] md:w-auto ${openKey === "report" ? "opened bg-[#05547E]" : ""}`}
+                >
+                  รายงาน
+                </button>
+
+                {openKey === "report" && (
+                  <ul
+                    className={` ${mobileOpen ? "static" : "absolute"} top-full left-0 min-w-[220px] bg-[#05547E]`}
+                  >
+                    <li>
+                      <a
+                        href="#กราฟ"
+                        onClick={closeAll}
+                        className="block cursor-pointer px-4 py-2 hover:bg-[#033A57]"
+                      >
+                        กราฟ
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#รายงานภาพรวม"
+                        onClick={closeAll}
+                        className="block cursor-pointer px-4 py-2 hover:bg-[#033A57]"
+                      >
+                        รายงานภาพรวม
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#รายงานรายโครงการ"
+                        onClick={closeAll}
+                        className="block cursor-pointer px-4 py-2 hover:bg-[#033A57]"
+                      >
+                        รายงานรายโครงการ
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#ประวัติบุคคล/หน่วยงาน"
+                        onClick={closeAll}
+                        className="block cursor-pointer px-4 py-2 hover:bg-[#033A57]"
+                      >
+                        ประวัติบุคคล/หน่วยงาน
+                      </a>
+                    </li>
+                  </ul>
+                )}
+              </li>
+              <li>
+                <a
+                  href="#คู่มือ"
+                  onClick={closeAll}
+                  className="flex h-[46px] cursor-pointer items-center px-[20px] hover:bg-[#05547E]"
+                >
+                  คู่มือ
+                </a>
+              </li>
+              <li className="relative">
+                <button
+                  type="button"
+                  onClick={() => toggle("user")}
+                  className={`subMenu flex h-[46px] w-full cursor-pointer items-center px-[20px] hover:bg-[#05547E] md:w-auto ${openKey === "user" ? "opened bg-[#05547E]" : ""}`}
+                >
+                  จัดการผู้ใช้
+                </button>
+
+                {openKey === "user" && (
+                  <ul
+                    className={` ${mobileOpen ? "static" : "absolute"} top-full left-0 min-w-[220px] bg-[#05547E]`}
+                  >
+                    <li>
+                      <a
+                        href="#จัดการผู้ใช้ภายใน"
+                        onClick={closeAll}
+                        className="block cursor-pointer px-4 py-2 hover:bg-[#033A57]"
+                      >
+                        จัดการผู้ใช้ภายใน
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#จัดการผู้ใช้ภายนอก"
+                        onClick={closeAll}
+                        className="block cursor-pointer px-4 py-2 hover:bg-[#033A57]"
+                      >
+                        จัดการผู้ใช้ภายนอก
+                      </a>
+                    </li>
+                  </ul>
+                )}
+              </li>
+              <li className="relative">
+                <button
+                  type="button"
+                  onClick={() => toggle("settings")}
+                  className={`subMenu flex h-[46px] w-full cursor-pointer items-center px-[20px] hover:bg-[#05547E] md:w-auto ${openKey === "settings" ? "opened bg-[#05547E]" : ""}`}
+                >
+                  ตั้งค่าระบบ
+                </button>
+
+                {openKey === "settings" && (
+                  <ul
+                    className={` ${mobileOpen ? "static" : "absolute"} top-full left-0 min-w-[220px] bg-[#05547E]`}
+                  >
+                    <li>
+                      <a
+                        href="#Tags ข้อเสนอโครงการ"
+                        onClick={closeAll}
+                        className="block cursor-pointer px-4 py-2 hover:bg-[#033A57]"
+                      >
+                        Tags ข้อเสนอโครงการ
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#สถานะภาพ"
+                        onClick={closeAll}
+                        className="block cursor-pointer px-4 py-2 hover:bg-[#033A57]"
+                      >
+                        สถานะภาพ
+                      </a>
+                    </li>
+                  </ul>
+                )}
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+}
